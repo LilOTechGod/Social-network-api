7137db871dd63af8c51784533e2b25b1dc52eff6
unknown <perezoscar360@gmail.com>
2022-12-05 18:29:56 -0600

    found bug inside of index.js and structured my user and thought routes different

--- routes/api/ThoughtRoutes.js
+++ routes/api/ThoughtRoutes.js
@@ -10,12 +10,19 @@ const {
     deleteReaction,
 } = require('../../controllers/thoughtController');
 
-router.route('/').get(getThought).post(createThought);
+router.route('/')
+.get(getThought)
+.post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
 
-router.route('/:thoughtId/reactions').post(createReaction);
+router.route('/:thoughtId/reactions')
+.post(createReaction);
 
-router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
 
 module.exports = router;
